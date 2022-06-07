1fd1ab3e6aa0557f882f1ff755a6bb776e4dc863
m.vladimir <m.vladimir@codewizard.team>
2022-06-07 23:24:23 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { BlockchainStore, StoreProvider } from '../src/store'
+
+const store = new BlockchainStore()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider store={store}>
+      <Component {...pageProps} />
+    </StoreProvider>
+  )
 }
 
 export default MyApp
